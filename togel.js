f2f04558bb55613a4dc29140bc33ee8ef441bddc
spuc3 <rdokarlos@gmail.com>
2026-06-26 00:45:32 +0700

    Update togel.js

--- togel.js
+++ togel.js
@@ -519,6 +519,6 @@
   };
 
   window.openHelp = function () {
-    window.open("https://direct.lc.chat/19752556/", "_blank");
+    window.open("https://direct.lc.chat/19812376/", "_blank");
   };
 })();
